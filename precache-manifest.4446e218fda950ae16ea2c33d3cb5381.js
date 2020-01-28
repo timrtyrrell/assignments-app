684360a6150f2becf9ec20a08ccb6e30382292af
Tim Tyrrell <trt3ym@virginia.edu>
2020-01-28 17:23:13 -0500

    Updates

---
+++ precache-manifest.4446e218fda950ae16ea2c33d3cb5381.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0bcc7569ebd6c037b224100f073182",
+    "url": "/assignments-app/index.html"
+  },
+  {
+    "revision": "52affaa975ec7ffad049",
+    "url": "/assignments-app/static/css/main.2cec7829.chunk.css"
+  },
+  {
+    "revision": "429b6573952592c6b24d",
+    "url": "/assignments-app/static/js/2.9b36ebc8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/assignments-app/static/js/2.9b36ebc8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "52affaa975ec7ffad049",
+    "url": "/assignments-app/static/js/main.f47be7c1.chunk.js"
+  },
+  {
+    "revision": "bd61f2319184b4c3cf17",
+    "url": "/assignments-app/static/js/runtime-main.d77bece0.js"
+  }
+]);
